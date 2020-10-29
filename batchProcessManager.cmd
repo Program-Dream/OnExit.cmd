47fb87a23acd8f42ceb505b28b9091c5592c4ea7
Tim Greller <timlg07@gmail.com>
2020-10-29 18:04:34 +0100

    [bpm] Basic concept finished.

--- batchProcessManager.cmd
+++ batchProcessManager.cmd
@@ -1,9 +1,21 @@
-if "%~1"=="" echo:No Parameter given. & pause & exit
-::if not exists "%~f1" echo:The parameter is not valid file path. & pause & exit
+@echo off
+setlocal enableDelayedExpansion
 
-set "title=bpm[%~n1_%date%_%time%_%random%]"
-title %title%
-for /F "tokens=2 usebackq" %%P IN (`tasklist /NH /FI "WINDOWTITLE eq %title%*"`) do set "PID=%%P"
+  :: Checking the parameters
+  if "%~2"=="" echo:Not enough parameters given. (2 required) & pause & exit
+  if not exist "%~1" echo:The first parameter is not valid file path. & pause & exit
+  if not exist "%~2" echo:The second parameter is not valid file path. & pause & exit
 
-wscript runInBackground.vbs "OnExitListener.cmd %PID%" //nologo
-pause
+  :: Recieving the PID of the batchProcessManager instance itself.
+  set "title=bpm[%~n1_%date%_%time%_%random%]"
+  title %title%
+  for /F "tokens=2 usebackq" %%P IN (`tasklist /NH /FI "WINDOWTITLE eq %title%*"`) do set "PID=%%P"
+
+  :: Activating the listener process.
+  wscript runInBackground.vbs "OnExitListener.cmd %PID%" //nologo
+
+endlocal
+
+:: Execute main program now.
+cd /d "%~dp1"
+%1
