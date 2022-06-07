d48a914ef3ae5fa0f93d242ec6b13fe546a3fb7b
Illegal-Services <BUZZARDGTA@protonmail.com>
2022-06-07 13:48:02 +0200

    improved shell compatibility

--- batchProcessManager.cmd
+++ batchProcessManager.cmd
@@ -1,22 +1,31 @@
 @echo off
 setlocal enableDelayedExpansion
 
-  :: Checking if 2 parameters were given and if the first one is a valid file.
-  if "%~2"=="" echo:Not enough parameters given. (2 required) & pause & exit /b 1
-  if not exist "%~1" echo:The given application does not exist. & pause & exit /b 2
-  if not exist "%~2" echo:The given onExit action script does not exist. & pause & exit /b 3
+    :: Checking if 2 parameters were given and if the first one is a valid file.
+    if "%~2"=="" echo Not enough parameters given. (2 required) & pause & exit /b 1
+    if not exist "%~1" echo The given application does not exist. & pause & exit /b 2
+    if not exist "%~2" echo The given onExit action script does not exist. & pause & exit /b 3
 
-  :: Recieving the PID of the batchProcessManager instance itself.
-  set "title=bpm[%~n1_%date%_%time%_%random%]"
-  title %title%
-  for /F "tokens=2 usebackq" %%P IN (`tasklist /NH /FI "WINDOWTITLE eq %title%*"`) do set "PID=%%P"
+    :: Recieving the PID of the batchProcessManager instance itself.
+    set "bmp_random=%date%_%time%_%random%"
+    set "bmp_random_regex=!bmp_random!"
+    set "bmp_random_regex=!bmp_random_regex:/=\/!"
+    set "bmp_random_regex=!bmp_random_regex::=\:!"
+    title bpm[!bmp_random!]
+    for %%A in (cmd.exe WindowsTerminal.exe conhost.exe) do (
+        for /f "tokens=2delims=," %%B in (
+            '2^>nul tasklist /v /nh /fo csv /fi "imagename eq %%A" ^| findstr /rxc:".*,\".*bpm\[!bmp_random_regex!\].*\""'
+        ) do (
+            set "PID=%%~B"
+        )
+    )
 
-  :: Changing the title to a less cryptic one, in case the application 
-  :: decides to use the default title.
-  title %~n1 - BatchProcessManager.
+    :: Changing the title to a less cryptic one, in case the application
+    :: decides to use the default title.
+    title %~n1 - BatchProcessManager.
 
-  :: Activating the listener process.
-  wscript "%~dp0runInBackground.vbs" "%~dp0OnExitListener.cmd" %PID% "%~f2" //nologo
+    :: Activating the listener process.
+    wscript "%~dp0runInBackground.vbs" "%~dp0OnExitListener.cmd" !PID! "%~f2" //nologo
 
 endlocal
 
